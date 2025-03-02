9f3df93efb958d85fa9a9ed55f420fb7e060bbe9
Davi Oliveira <davioliveira.java@gmail.com>
2025-03-02 14:15:47 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 starship init fish | source
-set FNM_PATH "/home/davio/.local/share/fnm"
-if [ -d "$FNM_PATH" ]
-  set PATH "$FNM_PATH" $PATH
-	fnm env --use-on-cd --shell fish | source
-end
+~/.local/bin/mise activate fish | source
 alias cat=batcat
 set fish_greeting
+if not contains "$HOME/.local/bin:$HOME/bin:" $PATH
+    set PATH "$HOME/.local/bin:$HOME/bin:$PATH"
+end
+export PATH
