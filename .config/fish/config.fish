f7453030cf30a1f5a44a10e4d6818e68b0689958
Davi Oliveira <davioliveira.java@gmail.com>
2025-03-02 14:18:54 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 starship init fish | source
 ~/.local/bin/mise activate fish | source
-alias cat=batcat
+alias cat=batcat # review
+alias docker=podman
 set fish_greeting
 if not contains "$HOME/.local/bin:$HOME/bin:" $PATH
     set PATH "$HOME/.local/bin:$HOME/bin:$PATH"
