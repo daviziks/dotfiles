ee6393ff8f8f04a365058c78b00b99b3efb866d5
Davi Oliveira <davioliveira.java@gmail.com>
2025-03-03 01:00:15 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 starship init fish | source
 ~/.local/bin/mise activate fish | source
-alias cat=batcat
+alias cat=bat
 alias lzg=lazygit
 set fish_greeting
 set -U fish_user_paths $fish_user_paths $HOME/.local/bin
