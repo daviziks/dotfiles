3661ca0dece009daa14bd2697f6b87836157ad64
Davi Oliveira <davioliveira.java@gmail.com>
2025-06-30 11:18:06 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,8 @@
 starship init fish | source
-~/.local/bin/mise activate fish | source
+mise activate fish | source
 alias cat=bat
 alias lzg=lazygit
 alias ls="eza -1 --icons"
-alias vpn-on="wg-quick up ~/dev/vpn.conf"
-alias vpn-off="wg-quick down ~/dev/vpn.conf"
 set fish_greeting
 set -U fish_user_paths $fish_user_paths $HOME/.local/bin
 export PATH
