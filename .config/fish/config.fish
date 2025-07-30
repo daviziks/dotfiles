438427ecb9655caf4606cdc8563894ea9f3d2d1e
Davi Oliveira <davioliveira.java@gmail.com>
2025-07-30 14:53:33 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,4 +5,5 @@ alias lzg=lazygit
 alias ls="eza -1 --icons"
 set fish_greeting
 set -U fish_user_paths $fish_user_paths $HOME/.local/bin
+set -U fish_user_paths $fish_user_paths $HOME/.opencode/bin
 export PATH
