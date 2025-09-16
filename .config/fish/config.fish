598de00dcc010688e707a382270328f7d68bcccc
Davi Oliveira <davioliveira.java@gmail.com>
2025-09-16 19:08:16 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ alias lzg=lazygit
 alias ls="eza -1 --icons"
 alias lzd="lazydocker"
 alias pr="gh pr create -B develop -a '@me'"
+alias zed="zeditor"
 set fish_greeting
 set -U fish_user_paths $fish_user_paths $HOME/.local/bin
 set -U fish_user_paths $fish_user_paths $HOME/.opencode/bin
