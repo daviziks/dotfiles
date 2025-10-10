80118fab8ace3efec501effd34a051969d5f1835
Davi Oliveira <davioliveira.java@gmail.com>
2025-10-10 00:31:09 -0300

    Add new aliases for git and bun commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,9 @@ alias lzg=lazygit
 alias ls="eza -1 --icons"
 alias lzd="lazydocker"
 alias pr="gh pr create -B develop -a '@me'"
-alias zed="zeditor"
+alias cm="git commit -m"
+alias rb="git rebase develop"
+alias bt="bun test"
 set fish_greeting
 set -U fish_user_paths $fish_user_paths $HOME/.local/bin
 set -U fish_user_paths $fish_user_paths $HOME/.opencode/bin
